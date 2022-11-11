95052310b9daa7215c2bafab80785b2a0c2a3f0b
Andrei.Mesilov@urfu.me <Andrei.Mesilov@urfu.me>
2022-11-11 21:45:43 +0500

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// import {weatherMatch} from "./weather-match.js";
+// console.log(weatherMatch)
 const weatherMatch = {
     "0" : "./weather/icons8-sun-100.png",
     "1" : "./weather/icons8-sun-100.png",
@@ -27,50 +29,82 @@ const weatherMatch = {
     "96" : "./weather/icons8-storm-100.png",
     "99" : "./weather/icons8-storm-100.png",
 }
+const headers = new Headers();
+headers.set("Access-Control-Allow-Origin", "*")
 async function getWeather(id){
-    const coordinates = getCoordinates(id);
-    let test = document.getElementById(id)
-    console.log(test)
-    let widgets = document.getElementById("widgets")
-    widgets.innerHTML = ""
-    const fetchData = await fetchCoordinatesData(coordinates)
-    console.log(fetchData.weather)
-    addWidget(getWeatherImage(fetchData.weather.current_weather.weathercode));
-    addTextWidget(fetchData.weather.current_weather.temperature + "°");
-    addTextWidget("Скорость ветра: " + fetchData.weather.current_weather.windspeed)
-    addTextWidget(fetchData.time.time)
+    let appContainer = document.getElementById(id)
+    let widgets = appContainer.querySelector(".widgets-list");
+    widgets.innerHTML = "";
+    try{
+        const coordinates = getCoordinates(appContainer);
+        const fetchData = await fetchWeatherData(coordinates);
+        console.log(fetchData.weather);
+        addWidget(getWeatherImage(fetchData.weather.current_weather.weathercode), appContainer);
+        addTextWidget(fetchData.weather.current_weather.temperature + "°", appContainer);
+        addTextWidget(`Скорость ветра: ${fetchData.weather.current_weather.windspeed} м/с`, appContainer);
+        addTextWidget(fetchData.time.time, appContainer);
+        initializeMap(coordinates,widgets);
+    }
+    catch(err){
+        addTextWidget(err,appContainer);
+    }
 }
-function getCoordinates(id){
-    const latitude = document.getElementById("latitude").value.replace(',','.')
-    const longitude = document.getElementById("longitude").value.replace(',','.')
+function getCoordinates(appContainer){
+    const latitude = appContainer.querySelector("input[name='latitude']").value.replace(',','.');
+    const longitude = appContainer.querySelector("input[name='longitude']").value.replace(',','.');
     if ((isNaN(latitude) || isNaN(parseFloat(latitude)))
      || (isNaN(longitude)) || isNaN(parseFloat(longitude))){
-        throw new Error("Incorrect input")
+        throw new Error("Incorrect input");
     }
     else{
-        return {latitude, longitude}
+        return {latitude, longitude};
     }
 }
 function getWeatherImage(weathercode){
     const weatherImage = new Image(100,100);
     weatherImage.src = weatherMatch[weathercode];
+    weatherImage.style = "display: inline-block"
     return weatherImage;
 }
-function addTextWidget(text){
+function addTextWidget(text, appContainer){
     const element = document.createElement("p");
     element.style.textAlign = "center";
     element.textContent = text;
-    addWidget(element);
+    addWidget(element,appContainer);
 }
-function addWidget(htmlChild){
+function addWidget(htmlChild, appContainer){
     const li = document.createElement("li");
-    li.appendChild(htmlChild)
-    document.getElementById("widgets").appendChild(li);
+    li.appendChild(htmlChild);
+    appContainer.querySelector(".widgets-list").appendChild(li);
 }
-async function fetchCoordinatesData(coordinates){
-    const [weather, time] = await Promise.all([
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current_weather=true&weathercode`).then((res) => res.json()),
-        fetch(`https://timeapi.io/api/Time/current/coordinate?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}`).then((res) => res.json())
-    ]);
-    return {weather,time}
+async function fetchWeatherData(coordinates){
+        const [weather, time] = await Promise.all([
+            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current_weather=true&weathercode`, {mode: "cors", headers: {'Access-Control-Allow-Origin':'*'}})
+            .then((res) =>{
+                if (res.ok){
+                    return res.json()
+                }
+                throw new Error("Ошибка соединения")
+            }),
+            fetch(`https://timeapi.io/api/Time/current/coordinate?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}`,{mode: "cors", headers: {'Access-Control-Allow-Origin':'*'}})
+            .then((res) => {
+                if (res.ok){
+                    return res.json()
+                }
+                throw new Error("Ошибка соединения")
+            })
+        ]);
+
+        return {weather,time}
     }
+function initializeMap(coordinates, widgets){
+    const ymap = document.createElement("div")
+    ymap.className = "ymap"
+    widgets.appendChild(ymap)
+    var myMap = new ymaps.Map(ymap, {
+        center: [coordinates.latitude, coordinates.longitude],
+        zoom: 8,
+    });
+    var myPlaceMark = new ymaps.Placemark([coordinates.latitude, coordinates.longitude])
+    myMap.geoObjects.add(myPlaceMark)
+}
