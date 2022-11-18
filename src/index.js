7360d9d8f391042e4254fbcc59fc9fcdb8557815
Drewik <yura.skleroznik@gmail.com>
2022-11-18 18:55:58 +0500

    first release

--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
+let widgetCounter = 1;
+
+// Скрипт для включения cors прокси
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
 async function getWeather(id){
-    let appContainer = document.getElementById(id)
-    let widgets = appContainer.querySelector(".widgets-list");
+    let widgetContainer = document.getElementById(`weather-${id}`)
+    let widgets = widgetContainer.querySelector(".widgets-list");
     widgets.innerHTML = "";
+    addWeatherElements(widgetContainer, widgets)
+}
+async function addWeatherElements(widgetContainer, widgets){
     try{
-        const coordinates = getCoordinates(appContainer);
-        addTextWidget("Загрузка данных...", appContainer);
+        const coordinates = getCoordinates(widgetContainer);
+        addTextWidget("Загрузка данных...", widgetContainer);
         const fetchData = await fetchWeatherData(coordinates);
         widgets.innerHTML = "";
-        addWidget(getWeatherImage(fetchData.weather.current_weather.weathercode), appContainer);
-        addTextWidget(fetchData.weather.current_weather.temperature + "°", appContainer);
-        addTextWidget(`Скорость ветра: ${fetchData.weather.current_weather.windspeed} м/с`, appContainer);
-        addTextWidget(fetchData.time.time, appContainer);
+        addWidget(getWeatherImage(fetchData.weather.current_weather.weathercode), widgetContainer);
+        addTextWidget(fetchData.weather.current_weather.temperature + "°", widgetContainer);
+        addTextWidget(`Скорость ветра: ${fetchData.weather.current_weather.windspeed} м/с`, widgetContainer);
+        addTextWidget(fetchData.time.time, widgetContainer);
         initializeMap(coordinates,widgets);
     }
     catch(err){
         widgets.innerHTML = "";
-        addTextWidget(err,appContainer);
+        addTextWidget(err,widgetContainer);
     }
 }
-function getCoordinates(appContainer){
-    const latitude = appContainer.querySelector("input[name='latitude']").value.replace(',','.');
-    const longitude = appContainer.querySelector("input[name='longitude']").value.replace(',','.');
+function getCoordinates(widgetContainer){
+    const latitude = widgetContainer.querySelector("input[name='latitude']").value.replace(',','.');
+    const longitude = widgetContainer.querySelector("input[name='longitude']").value.replace(',','.');
     if ((isNaN(latitude) || isNaN(parseFloat(latitude)))
      || (isNaN(longitude)) || isNaN(parseFloat(longitude))){
-        throw new Error("Incorrect input");
+        throw new Error("Введенные данные некорректны");
     }
     else{
         return {latitude, longitude};
@@ -35,27 +58,27 @@ function getWeatherImage(weathercode){
     weatherImage.style = "display: inline-block";
     return weatherImage;
 }
-function addTextWidget(text, appContainer){
+function addTextWidget(text, widgetContainer){
     const element = document.createElement("p");
     element.style.textAlign = "center";
     element.textContent = text;
-    addWidget(element,appContainer);
+    addWidget(element,widgetContainer);
 }
-function addWidget(htmlChild, appContainer){
+function addWidget(htmlChild, widgetContainer){
     const li = document.createElement("li");
     li.appendChild(htmlChild);
-    appContainer.querySelector(".widgets-list").appendChild(li);
+    widgetContainer.querySelector(".widgets-list").appendChild(li);
 }
 async function fetchWeatherData(coordinates){
         const [weather, time] = await Promise.all([
-            fetch(`https://proxy.cors.sh/https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current_weather=true&weathercode`)
+            fetch(`https://cors-anywhere.herokuapp.com/https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&current_weather=true&weathercode`)
             .then((res) =>{
                 if (res.ok){
                     return res.json()
                 }
                 throw new Error(res.status)
             }),
-            fetch(`https://proxy.cors.sh/https://timeapi.io/api/Time/current/coordinate?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}`)
+            fetch(`https://cors-anywhere.herokuapp.com/https://timeapi.io/api/Time/current/coordinate?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}`)
             .then((res) => {
                 if (res.ok){
                     return res.json()
@@ -77,6 +100,24 @@ function initializeMap(coordinates, widgets){
     });
     var myPlaceMark = new ymaps.Placemark([coordinates.latitude, coordinates.longitude])
     myMap.geoObjects.add(myPlaceMark)
+}
+
+function addWeatherApp(){
+    widgetCounter += 1
+    container = document.getElementById('weather-app-container')
+    container.insertAdjacentHTML('beforeend', `
+    <div class="widget-container" id="weather-${widgetCounter}">
+        <div class="input">
+            <p class="input-name">Широта<input class="input-field" type="text" name="latitude"></p>
+            <p class="input-name">Долгота<input class="input-field" type="text" name="longitude" style="margin-left: 41px"></p>
+            <button class="button" onclick="getWeather('${widgetCounter}')">Показать погоду</button>
+        </div>
+        <div class="widgets">
+            <ul class="widgets-list">
+                
+            </ul>
+        </div>
+    </div>`)
 }
 
 const weatherMatch = {
